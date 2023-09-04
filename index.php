d11915678c68be6a0d1d1be5dbb96616c235b975
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-04 12:05:24 +0200

    Mise en page rapide du produit

--- index.php
+++ index.php
@@ -19,8 +19,9 @@ $photo1="https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain
     </header>
 
     <div class="content">
-        <article class="produit"><?php echo $prix1," € ",$nom1;
-            ?>
+        <article class="produit">
+            <h3><?php echo $prix1," € ",$nom1;
+            ?></h3>
             <img src="<?php echo $photo1 ?>" alt="photo du nain">
         </article>
         <article></article>
