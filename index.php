de1b468478437d8b159b1583674f346d2fcdd72f
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-04 10:59:34 +0200

    Balises html

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-on est la
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="Projet"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet</title>
+</head>
+<body>
+
+</body>
+</html>
