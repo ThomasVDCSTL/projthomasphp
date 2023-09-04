c1c11707361e3e693c3c0bda9055a3d6970b53a7
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-04 14:08:10 +0200

    séparation des balises html

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $nom_page="Projet PHP Thomas";
+?>
+
+
+
+<?php include 'head.php';?>
+
+
+    <?php include 'header.php';?>
+    <div class="content">
+        <?php include 'item.php';?>
+    </div>
+<?php include 'footer.php';?>
+
+
--- item.php
+++ item.php
@@ -3,31 +3,9 @@ $prix1=45;
 $nom1="le naing";
 $photo1="https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain-de-jardin-exterieur-impermeabiliser-nain-de-j.jpg";
 ?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="Projet"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Projet</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <header>
-        <div></div>
-    </header>
 
-    <div class="content">
-        <article class="produit">
-            <h3><?php echo $prix1," € ",$nom1;
-            ?></h3>
-            <img src="<?php echo $photo1 ?>" alt="photo du nain">
-        </article>
-        <article></article>
-        <article></article>
-    </div>
-
-</body>
-</html>
-
+<article class="produit">
+    <h3><?php echo $prix1," € ",$nom1;
+        ?></h3>
+    <img src="<?php echo $photo1 ?>" alt="photo du nain">
+</article>
