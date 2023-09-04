40cea77d6749542b86fe19e0d1b7c4e2bef76501
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-04 11:49:21 +0200

    premier article + création feuille css

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$prix1=45;
+$nom1="le naing";
+$photo1="https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain-de-jardin-exterieur-impermeabiliser-nain-de-j.jpg";
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -6,8 +11,22 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projet</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <header>
+        <div></div>
+    </header>
+
+    <div class="content">
+        <article class="produit"><?php echo $prix1," € ",$nom1;
+            ?>
+            <img src="<?php echo $photo1 ?>" alt="photo du nain">
+        </article>
+        <article></article>
+        <article></article>
+    </div>
 
 </body>
 </html>
+
