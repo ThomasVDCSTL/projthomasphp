1f8f7fae80fb0a55afb12fa97ed1eff20a0b3d6a
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-06 10:48:36 +0200

    ajout boucle foreach produit

--- item.php
+++ item.php
@@ -1,35 +1,26 @@
 <?php
-$articles = array( "Schmongus" => array("prix"=>45,
-                                        "photo"=>"https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain-de-jardin-exterieur-impermeabiliser-nain-de-j.jpg",
-                                        "description"=>'"He protec ..."',
-                                        "nom"=>"Schmongus"),
-                    "Steph" => array("prix"=>450,
-                        "photo"=>"https://commentseruiner.com/32058-large_default/nain-de-jardin-i-m-sexy.jpg",
-                        "description"=>'"... he attac"',
-                        "nom"=>"Steph")
+$articles = array(
+        "Schmongus" => array(
+            "prix"=>45,
+            "photo"=>"https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain-de-jardin-exterieur-impermeabiliser-nain-de-j.jpg",
+            "description"=>'"He protec ..."'
+        ),
+        "Steph" => array(
+            "prix"=>450,
+            "photo"=>"https://commentseruiner.com/32058-large_default/nain-de-jardin-i-m-sexy.jpg",
+            "description"=>'"... he attac"'
+        )
 );
 
-$prix1=45;
-$nom1="Schmongus Egide de Fer";
-$photo1="https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain-de-jardin-exterieur-impermeabiliser-nain-de-j.jpg";
-$description1='"He protec ..."';
-$prix2=450;
-$nom2="l'autre naing";
-$photo2="https://commentseruiner.com/32058-large_default/nain-de-jardin-i-m-sexy.jpg";
-$description2='"... he attac"';
+foreach ($articles as $nom => $article):?>
+    <div class="produit">
+        <h3><?php echo $article["prix"]," € ",$nom;
+            ?></h3>
+        <p><?php echo $article["description"]?></p>
+        <img src="<?php echo $article["photo"] ?>" alt="photo de<?php echo $nom?>">
+    </div>
+<?php endforeach;
+
 
 ?>
 
-<div class="produit" id="produit1">
-    <h3><?php echo $articles["Schmongus"]["prix"]," € ",$articles["Schmongus"]["nom"];
-        ?></h3>
-    <p><?php echo $articles["Schmongus"]["description"]?></p>
-    <img src="<?php echo $articles["Schmongus"]["photo"] ?>" alt="photo du nain protec">
-</div>
-
-<div class="produit" id="produit2">
-    <h3><?php echo $articles["Steph"]["prix"]," € ",$articles["Steph"]["nom"];
-        ?></h3>
-    <p><?php echo $articles["Steph"]["description"]?></p>
-    <img src="<?php echo $articles["Steph"]["photo"] ?>" alt="photo du nain attac">
-</div>
