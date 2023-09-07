9d9c7de35e5e77f4977f8b7aedc4f0ae85bb1c15
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-07 13:47:20 +0200

    pas sur de ce que j'ai fait... un bouton je crois

--- fonctions.php
+++ fonctions.php
@@ -1,13 +1,20 @@
 <?php
 global $articles;
-function display_articles()
+function display_articles(): void
 {
     global $articles;
     foreach ($articles as $nom => $article):?>
         <div class="produit">
-            <h3><?php echo $nom;
-                ?></h3>
-            <p><?php echo $article["description"] ?></p>
+            <div class="texte_produit">
+                <div class="gras">
+                    <p><?php echo $nom; ?></p>
+                    <p><?php echo formatPrice($article["prix"]); ?></p>
+                </div>
+                <p><?php echo $article["description"] ?></p>
+                <button <?php if ($article["quantité"] < 1): ?>disabled<?php endif;
+                ?>>Ajouter<br>au panier
+                </button>
+            </div>
             <img src="<?php echo $article["photo"] ?>" alt="photo de<?php echo $nom ?>">
         </div>
     <?php endforeach;
@@ -15,7 +22,7 @@ function display_articles()
 
 function formatPrice(float $price): string
 {
-    return number_format($price / 100, 2, ",", " ") . ' €';
+    return number_format($price, 2, ",", " ") . ' €';
 }
 
 //echo formatPrice(1000);
--- item.php
+++ item.php
@@ -3,13 +3,19 @@ $articles = array(
         "Schmongus" => array(
             "prix"=>45,
             "photo"=>"https://www.cdiscount.com/pdt2/8/9/6/1/350x350/auc7745647210896/rw/nain-de-jardin-exterieur-impermeabiliser-nain-de-j.jpg",
-            "description"=>'"He protec ..."'
+            "description"=>'"He protec ..."',
+            "quantité"=>12,
         ),
         "Steph" => array(
             "prix"=>450,
             "photo"=>"https://commentseruiner.com/32058-large_default/nain-de-jardin-i-m-sexy.jpg",
-            "description"=>'"... he attac"'
+            "description"=>'"... he attac"',
+            "quantité"=>0,
         )
+);
+
+$panier= array(
+
 );
 
 
