c5a4f4ea7b54576c5625341a00790ae493d2d1d4
ThomasVDCSTL <thomas.vandecasteele@le-campus-numerique.fr>
2023-09-08 10:46:18 +0200

    ouais grave de fou

--- fonctions.php
+++ fonctions.php
@@ -11,9 +11,15 @@ function display_articles(): void
                     <p><?php echo formatPrice($article["prix"]); ?></p>
                 </div>
                 <p><?php echo $article["description"] ?></p>
-                <button <?php if ($article["quantité"] < 1): ?>disabled<?php endif;
-                ?>>Ajouter<br>au panier
-                </button>
+                <div class="ajout_panier">
+                    <form action="item.php" method="get">
+                        <label class="quantity" for="quantity">Quantité : </label><input type="number" max="99" min="1" value="1">
+                    </form>
+                    <button <?php if ($article["quantité"] < 1): ?>disabled class="indispo">Produit<br>indisponible<?php else:
+                    ?> class="dispo" >Ajouter<br>au panier<?php endif;
+                    ?>
+                    </button>
+                </div>
             </div>
             <img src="<?php echo $article["photo"] ?>" alt="photo de<?php echo $nom ?>">
         </div>
--- panier_piano.php
+++ panier_piano.php
@@ -5,13 +5,13 @@ $nom_page="Panier";
 <?php include 'header.php';?>
     <h2>Votre Panier :</h2>
     <div class="piano">
-        <div class="panier">
+        <div class="infos">
             <h3>Informations de livraison</h3>
             <div class="formulaire">
 
             </div>
         </div>
-        <div class="total">
+        <div class="panier">
             <h3>Panier</h3>
 
         </div>
